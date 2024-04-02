3d030546a864475c3396e7ec1302864d6e9aa157
Luiz Fernando <145511428+Plistercomet@users.noreply.github.com>
2024-04-02 00:16:25 -0300

    testando service-worker do professor

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,47 +1,68 @@
-// This is the "Offline page" service worker
+var cacheName = 'pwaTeste+-v1.0';
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+self.addEventListener('install', event => {
 
-const CACHE = "pwabuilder-page";
+  self.skipWaiting();
 
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "offline.html";
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => cache.addAll([
 
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
+        './index.html',
+
+        './assets/css/bootstrap.min.css',
+
+        './assets/js/bootstrap.min.js',
+
+        './assets/js/jquery.min.js',
+
+        './assets/js/popper.min.js',
+
+        './assets/img/background.png',
+        './assets/img/favicon.png',
+        './assets/img/logo.png',
+        './assets/img/icon_128.png',
+        './assets/img/icon_144.png',
+        './assets/img/icon_152.png',
+        './assets/img/icon_167.png',
+        './assets/img/icon_180.png',
+        './assets/img/icon_192.png',
+        './assets/img/icon_256.png',
+        './assets/img/icon_512.png',
+        './assets/img/formulas.JPG',
+
+        './assets/img/batman.jpeg',
+        './assets/img/chefao.jpeg',
+        './assets/img/leao.jpeg',
+      ]))
+  );
+});
+
+self.addEventListener('message', function (event) {
+  if (event.data.action === 'skipWaiting') {
     self.skipWaiting();
   }
 });
 
-self.addEventListener('install', async (event) => {
-  event.waitUntil(
-    caches.open(CACHE)
-      .then((cache) => cache.add(offlineFallbackPage))
-  );
-});
+self.addEventListener('fetch', function (event) {
+  //Atualizacao internet
+  event.respondWith(async function () {
+    try {
+      return await fetch(event.request);
+    } catch (err) {
+      return caches.match(event.request);
+    }
+  }());
 
-if (workbox.navigationPreload.isSupported()) {
-  workbox.navigationPreload.enable();
-}
-
-self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        const preloadResp = await event.preloadResponse;
-
-        if (preloadResp) {
-          return preloadResp;
+  //Atualizacao cache
+  /*event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
         }
+        return fetch(event.request);
+      })
+  );*/
 
-        const networkResp = await fetch(event.request);
-        return networkResp;
-      } catch (error) {
-
-        const cache = await caches.open(CACHE);
-        const cachedResp = await cache.match(offlineFallbackPage);
-        return cachedResp;
-      }
-    })());
-  }
 });
